4f3bc2afd8aac80b3d8e09b267a75c10c4d437bf
flmarsou <marsouinflorent12@gmail.com>
2025-09-18 09:43:35 +0200

    Added MariaDB script for initialization and setup

---
+++ srcs/requirements/MariaDB/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	echo "[ INFO ] Initializing MariaDB..."
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+
+	echo "[ INFO ] Setting up MariaDB..."
+	mysqld --user=mysql --bootstrap <<EOF
+CREATE DATABASE IF NOT EXISTS \'${SQL_DATABASE}\';
+CREATE USER IF NOT EXISTS \'${SQL_USER}\'@'localhost' IDENTIFIED BY '${SQL_PASSWORD}';
+GRANT ALL PRIVILEGES ON \'${SQL_DATABASE}\'.* TO \'${SQL_USER}\@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';
+FLUSH PRIVILEGES;
+EOF
+fi
+
+exec mysqld_safe --user=mysql
